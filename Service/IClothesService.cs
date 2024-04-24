70aa28cdad049a01fbd869b8edd5656d42f8c5d6
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-04-24 23:10:18 +0500

    Structure

---
+++ Service/IClothesService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothesShop.Service
+{
+    internal interface IClothesService
+    {
+    }
+}
