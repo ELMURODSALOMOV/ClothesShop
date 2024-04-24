7480e08448a06378a80347e85301b237113d2207
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-04-25 00:18:26 +0500

    List Create

--- Broker/Storeage/IStoreageBroker.cs
+++ Broker/Storeage/IStoreageBroker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClothesShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace ClothesShop.Broker.Storeage
 {
     internal interface IStoreageBroker
     {
+        List<Clothes> GetAllClothes();
+        Clothes GetClothes(int id);
+        bool DeleteClothes(int id);
+        Clothes UpdateClothes(int id, Clothes clothes);
+        void AddClothes(Clothes clothes);
+        void AddRangeClothes(List<Clothes> clothes);
+        void PurchaseClothes(string model);
+        List<Clothes> SoldInformationClothes();
+
     }
 }
--- Broker/Storeage/ListStoreageBroker.cs
+++ Broker/Storeage/ListStoreageBroker.cs
@@ -9,13 +9,83 @@ namespace ClothesShop.Broker.Storeage
 {
     internal class ListStoreageBroker : IStoreageBroker
     {
+        List<Clothes> demoClothes = new List<Clothes>();
         List<Clothes> clothes = new List<Clothes>();
         public ListStoreageBroker()
         {
-            clothes[0] = new Clothes();
+            clothes[0] = new Clothes()
             {
-                
-            }
+                Id = 1,
+                Model = "GUCCI",
+                Type = ClothesType.PoloShirt,
+                Cost = 100000,
+                Size = 58,
+                Color = "Red",
+                Discraption = "Cotton, XL",
+                Amount = 20
+            };
+            clothes[1] = new Clothes()
+            {
+                Id = 2,
+                Model = "LACOSTI",
+                Type = ClothesType.Jeans,
+                Cost = 120000,
+                Size = 55,
+                Color = "Blue",
+                Discraption = "Cotton 50%, synthetics 50%, XXl",
+                Amount = 16
+            };
+            clothes[2] = new Clothes()
+            {
+                Id = 3,
+                Model = "Zara",
+                Type = ClothesType.Shoes,
+                Cost = 200000,
+                Size = 60,
+                Color = "Black",
+                Discraption = "Skin 100%, 60",
+                Amount = 23
+            };
+        }
+
+        public void AddClothes(Clothes clothes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRangeClothes(List<Clothes> clothes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteClothes(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clothes> GetAllClothes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clothes GetClothes(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PurchaseClothes(string model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clothes> SoldInformationClothes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clothes UpdateClothes(int id, Clothes clothes)
+        {
+            throw new NotImplementedException();
         }
     }
 }
