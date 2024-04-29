e6ec5f39a6ca4b486465500a9d480992f6db5f3a
AslanbekHasanov <learneraslanbek1220@gmail.com>
2024-04-29 16:25:46 +0500

    Validation Read All Clothes

--- Program.cs
+++ Program.cs
@@ -1,29 +1,6 @@
 ﻿
-using ClothesShop.Broker.Storeage;
-using ClothesShop.Models;
 
-ListStoreageBroker listStoreageBroker = new ListStoreageBroker();
-foreach (var storeage in listStoreageBroker.GetAllClothes())
-{
-    Console.WriteLine("Id " + storeage.Id);
-    Console.WriteLine("Model " + storeage.Model);
-    Console.WriteLine("Type " + storeage.Type);
-    Console.WriteLine("Amount " + storeage.Amount);
-    Console.WriteLine("Discreption " + storeage.Discraption);
-    Console.WriteLine("Cost " + storeage.Cost);
-}
+using ClothesShop.Service;
 
-
-Console.WriteLine("==============");
-Console.Write("Enter type: ");
-string clothesType = Console.ReadLine();
-listStoreageBroker.PurchaseClothes(clothesType);
-Console.WriteLine("==============");
-
-foreach (var storeage in listStoreageBroker.SoldInformationClothes())
-{
-    Console.WriteLine("Id " + storeage.Id);
-    Console.WriteLine("Model " + storeage.Model);
-    Console.WriteLine("Amount " + storeage.Amount);
-    Console.WriteLine("Balance " + storeage.Balance);
-}
+IClothesService clothesService = new ClothesService();
+clothesService.ReadAllClothes();
