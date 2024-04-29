759b7785885819fccc63b37e42a5d4aac89f3df0
ELMURODSALOMOV <salomovelmurod062@gmail.com>
2024-04-29 15:11:49 +0500

    Read All Clothes

--- Service/ClothesService.cs
+++ Service/ClothesService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ClothesShop.Broker.Logging;
+using ClothesShop.Broker.Storeage;
+using ClothesShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,56 @@ using System.Threading.Tasks;
 
 namespace ClothesShop.Service
 {
-    internal class ClothesService
+    internal class ClothesService : IClothesService
     {
+        private readonly ILoggingBroker loggingBroker;
+        private readonly IStoreageBroker listStoreageBroker;
+        public ClothesService()
+        {
+            this.listStoreageBroker = new ListStoreageBroker();
+            this.loggingBroker = new LoggingBroker();
+        }
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertClothes(Clothes clothes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertRangeClothes(List<Clothes> clothes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Purchase(string model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clothes> ReadAllClothes()
+        {
+            List<Clothes> clothesList = new List<Clothes>();
+            if(clothesList.Count is 0)
+            {
+            }
+        }
+
+        public Clothes ReadClothes(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Clothes> SoldInformation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Clothes Update(int id, Clothes clothes)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Service/IClothesService.cs
+++ Service/IClothesService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ClothesShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,13 @@ namespace ClothesShop.Service
 {
     internal interface IClothesService
     {
-
+        List<Clothes> ReadAllClothes();
+        Clothes ReadClothes(int id);
+        bool Delete(int id);
+        Clothes Update(int id, Clothes clothes);
+        void InsertClothes(Clothes clothes);
+        void InsertRangeClothes(List<Clothes> clothes);
+        void Purchase(string model);
+        List<Clothes> SoldInformation();
     }
 }
